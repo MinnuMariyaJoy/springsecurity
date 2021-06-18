ddc601f1563916c220737f9b4d7ae66293225147
Minnu Mariya Joy <minnumariyaj@gmail.com>
2021-06-19 02:04:21 +0530

    done code for sending a simple mail

--- Spring-Email/src/main/java/com/myprojects/springemail/SpringEmailApplication.java
+++ Spring-Email/src/main/java/com/myprojects/springemail/SpringEmailApplication.java
@@ -1,13 +1,25 @@
-package com.myprojects.springemail;
+ package com.myprojects.springemail;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SpringEmailApplication {
+     @Autowired
+	private EmailSenderService service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEmailApplication.class, args);
+	}
+
+	@EventListener(ApplicationEvent.class)
+	public void triggerMail(){
+       service.sendSimpleEmail("demouser3101@gmail.com",
+			   "This is email body",
+			   "this is email subject");
 	}
 
 }
