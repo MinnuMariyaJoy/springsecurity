a7bfac6a0eace6d5f53d1ef4a462f43b93500a25
Minnu Mariya Joy <minnumariyaj@gmail.com>
2021-06-20 13:02:54 +0530

    done basics operations on flyway

---
+++ flyway/src/main/resources/db/migration/V4_4__insertData.sql
@@ -0,0 +1,2 @@
+INSERT INTO REGISTRATION_USER(username,first_name,last_name,email,mobile) VALUES ('ABC','Ab','Xy','abc@gamil.com',6282991668);
+INSERT INTO REGISTRATION_USER(username,first_name,last_name,email,mobile) VALUES ('DEF','SD','sfsf','sfds@gamil.com',6952991668);
